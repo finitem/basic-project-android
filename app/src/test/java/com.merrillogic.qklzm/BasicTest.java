5ddab863a159764a1e6eca4c50f27d80e69a1c9d
Merrill <merrill@merrillogic.com>
2015-07-18 17:52:29 -0700

    Basic project

---
+++ app/src/test/java/com.merrillogic.qklzm/BasicTest.java
@@ -0,0 +1,14 @@
+package com.merrillogic.qklzm;
+
+public class BasicTest {
+
+	@Before
+	public void reset() {
+		//Reset here
+	}
+
+	@Test
+	public void doBasicAlwaysPassingTest() {
+		//Weeee
+	}
+}
